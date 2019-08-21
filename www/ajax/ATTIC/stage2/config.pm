d73f44db6b61df55786780352c270db048463b3c
Andrew Martin <Andrew C.R. Martin>
2019-08-21 16:59:18 +0100

    Removed really unused ATTIC folder

--- www/ajax/ATTIC/stage2/config.pm
+++
@@ -1,6 +0,0 @@
-package config;
-
-#$tmpdir = "/tmp";
-$tmpdir = "/home/httpd/html/tmp";
-
-1;
--- www/ajax/ATTIC/stage3/config.pm
+++
@@ -1,7 +0,0 @@
-package config;
-
-#$tmpdir = "/tmp";
-#$tmpdir = "/home/httpd/html/tmp";
-$tmpdir = "/acrm/www/html/tmp";
-
-1;
