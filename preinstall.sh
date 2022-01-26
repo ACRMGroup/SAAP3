0d6f12e50ba7700dac5e0edb2b2dac8a0cd84316
AndrewCRMartin <andrew@bioinf.org.uk>
2022-01-26 18:45:56 +0000

    Added perl-Try-Tiny to preinstall

--- preinstall.sh
+++ preinstall.sh
@@ -5,6 +5,8 @@ packagemanager=yum
 sudo $packagemanager -y install R
 # Needed to download files
 sudo $packagemanager -y install perl-LWP-Protocol-https
+# Random Perl things
+sudo $packagemanager -y install perl-Try-Tiny
 # Needed for Muscle
 sudo $packagemanager -y install libstdc++-static glibc-static
 # Needed to read JSON files
